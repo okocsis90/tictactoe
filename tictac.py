474f4e27609550b84745e5c45af302032e317255
okocsis90 <o.kocsis90@gmail.com>
2016-10-19 09:54:23 +0200

    player2 wins done

--- tictac.py
+++ tictac.py
@@ -1,7 +1,7 @@
 
 board_global = [1,2,3,4,5,6,7,8,9]
 
-# board
+# board:
 def board_status(board):
     print(" ", board[6], " | ", board[7], " | ", board[8])
     print("-----------------")
@@ -9,55 +9,55 @@ def board_status(board):
     print("-----------------")
     print(" ", board[0], " | ", board[1], " | ", board[2])
 
-#what number did player 1 choose
-def player__what_number(player__pick, board_1, player):
+# print on the board what number did player choose:
+def player_what_number(player_pick, board_1, player):
     for i in range(len(board_1)):
-        if player__pick == board_1[i] and type(board_1[i]) is int:
+        if player_pick == board_1[i] and type(board_1[i]) is int:
             if player == "player1":               
                 board_1[i] = "x"
             else:
                 board_1[i] = "o"
-        #else:
-        #    print("1-9 please, choose an empty place")
 
-
-def player_01_wins(board_globalwin): 
-    if board_globalwin[0] == "x" and board_globalwin[1] == "x" and board_globalwin[2] == "x":
-        print("Player1 won!")
+# when a player wins:
+def player_wins(board_globalwin, player, sign): 
+    if board_globalwin[0] == sign and board_globalwin[1] == sign and board_globalwin[2] == sign:
+        print(player, "won!")
         return 1
-    elif board_globalwin[3] == "x" and board_globalwin[4] == "x" and board_globalwin[5] == "x":
-        print("Player1 won!")
+    elif board_globalwin[3] == sign and board_globalwin[4] == sign and board_globalwin[5] == sign:
+        print(player, "won!")
         return 1
-    elif board_globalwin[6] == "x" and board_globalwin[7] == "x" and board_globalwin[8] == "x":
-        print("Player1 won!")
+    elif board_globalwin[6] == sign and board_globalwin[7] == sign and board_globalwin[8] == sign:
+        print(player, "won!")
         return 1
-    elif board_globalwin[0] == "x" and board_globalwin[3] == "x" and board_globalwin[6] == "x":
-        print("Player1 won!")
+    elif board_globalwin[0] == sign and board_globalwin[3] == sign and board_globalwin[6] == sign:
+        print(player, "won!")
         return 1
-    elif board_globalwin[1] == "x" and board_globalwin[4] == "x" and board_globalwin[7] == "x":
-        print("Player1 won!")
+    elif board_globalwin[1] == sign and board_globalwin[4] == sign and board_globalwin[7] == sign:
+        print(player, "won!")
         return 1
-    elif board_globalwin[2] == "x" and board_globalwin[5] == "x" and board_globalwin[8] == "x":
-        print("Player1 won!")
+    elif board_globalwin[2] == sign and board_globalwin[5] == sign and board_globalwin[8] == sign:
+        print(player, "won!")
         return 1
-    elif board_globalwin[2] == "x" and board_globalwin[4] == "x" and board_globalwin[6] == "x":
-        print("Player1 won!")
+    elif board_globalwin[2] == sign and board_globalwin[4] == sign and board_globalwin[6] == sign:
+        print(player, "won!")
         return 1
-    elif board_globalwin[0] == "x" and board_globalwin[4] == "x" and board_globalwin[8] == "x":
-        print("Player1 won!")
+    elif board_globalwin[0] == sign and board_globalwin[4] == sign and board_globalwin[8] == sign:
+        print(player, "won!")
         return 1
 
+board_status(board_global)
 while True:
-    board_status(board_global)
     #player 01 picks a number
     player_01_location = int(input("Player1: Use your numerical keyboard to pick a location: "))
-    player__what_number(player_01_location, board_global, "player1")
+    player_what_number(player_01_location, board_global, "player1")
     board_status(board_global)
-    if player_01_wins(board_global) == 1:
+    if player_wins(board_global, "player1", "x") == 1:
         break
     #player 2 picks a number
     player_02_location = int(input("Player2: Use your numerical keyboard to pick a location: "))
-    player__what_number(player_02_location, board_global, "player2")
-
+    player_what_number(player_02_location, board_global, "player2")
+    board_status(board_global)
+    if player_wins(board_global, "player2", "o") == 1:
+        break
 
 
